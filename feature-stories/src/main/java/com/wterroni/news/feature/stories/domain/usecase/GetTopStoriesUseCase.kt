ec2d2bdaa0eb68e939ec904bfe754c29e5754fe1
Wellington Terroni <wellington.terroni@outlook.com>
2026-03-27 02:01:22 -0300

    chore: remove unnecessary inject annotation from use case

--- feature-stories/src/main/java/com/wterroni/news/feature/stories/domain/usecase/GetTopStoriesUseCase.kt
+++ feature-stories/src/main/java/com/wterroni/news/feature/stories/domain/usecase/GetTopStoriesUseCase.kt
@@ -2,9 +2,8 @@ package com.wterroni.news.feature.stories.domain.usecase
 
 import com.wterroni.news.feature.stories.domain.model.Story
 import com.wterroni.news.feature.stories.domain.repository.StoriesRepository
-import javax.inject.Inject
 
-class GetTopStoriesUseCase @Inject constructor(
+class GetTopStoriesUseCase(
     private val repository: StoriesRepository
 ) {
     suspend operator fun invoke(limit: Int, offset: Int): List<Story> {
